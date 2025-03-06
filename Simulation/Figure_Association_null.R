c78e88a81c9c1250547f317b03dae76455a7916e
ShunZhang0816 <3207042023@i.smu.edu.cn>
2025-03-06 19:10:12 +0800

    Update Figure_Association_null.R

--- Simulation/Figure_Association_null.R
+++ Simulation/Figure_Association_null.R
@@ -7,30 +7,30 @@ library(showtext)
 library(cowplot)
 library(tidyverse)
 
-dynastic_null   <- dynastic_null[dynastic_null$残差相关性 == 0.3,]
-population_null <- population_null[population_null$残差相关性 == 0.3,]
-none_null       <- none_null[none_null$残差相关性 == 0.3,]
+dynastic_null   <- dynastic_null[dynastic_null$rou == 0.3,]
+population_null <- population_null[population_null$rou == 0.3,]
+none_null       <- none_null[none_null$rou == 0.3,]
 
 bias_plot <- function(data,title){
-  bias <- ggplot(data, aes(x = 样本量, y = 直接效应Bias, group = 方法,
-                           shape = factor(方法, levels = c("FT-SEM","lm_parent","lm")),
-                           linetype = factor(方法, levels = c("FT-SEM","lm_parent","lm")))) +
+  bias <- ggplot(data, aes(x = Sample size, y = Offspring effect Bias, group = Method,
+                           shape = factor(Method, levels = c("FT-SEM","lm_parent","lm")),
+                           linetype = factor(Method, levels = c("FT-SEM","lm_parent","lm")))) +
     geom_point(size = 2) +  
     geom_line(linewidth = 0.8) +
     labs(x = "Sample size", y = "Bias",shape = "Method",linetype = "Method") +
     # theme_minimal() +
     theme(
-      panel.grid = element_blank(), # 去除背景的方框线
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  # 在面板周围添加黑色实线边框
-      panel.background = element_rect(fill = "white", color = NA),  # 设置面板背景为白色
+      panel.grid = element_blank(),
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8), 
+      panel.background = element_rect(fill = "white", color = NA),  
       axis.ticks = element_line(color = "black"),
-      axis.title = element_text(family = "sans",size = 9), # 加粗并增大轴标题字体
-      axis.text = element_text(family = "sans",size = 9),  # 增大坐标轴刻度字体
-      strip.text = element_text(family = "sans",size = 9), # 增大 facet 标题的字体大小并加粗
-      aspect.ratio = 1,  # 设置图形为正方形
-      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  # 设置标题居中，字体样式和大小
+      axis.title = element_text(family = "sans",size = 9), 
+      axis.text = element_text(family = "sans",size = 9),  
+      strip.text = element_text(family = "sans",size = 9), 
+      aspect.ratio = 1,  
+      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9), 
     ) +
-    scale_x_continuous(breaks = c(1000, 2000, 3000)) + # 设置横坐标刻度为1000、2000和3000
+    scale_x_continuous(breaks = c(1000, 2000, 3000)) + 
     scale_y_continuous(limits = c(-0.005,0.15)) +
     ggtitle(title)
   bias
@@ -40,25 +40,25 @@ bias_null_population <- bias_plot(population_null, "Residual population stratifi
 bias_null_none       <- bias_plot(none_null, "No bias")
 
 RMSE_plot <- function(data,title){
-  RMSE <- ggplot(data, aes(x = 样本量, y = 直接效应RMSE, group = 方法,
-                           shape = factor(方法, levels = c("FT-SEM","lm_parent","lm")),
-                           linetype = factor(方法, levels = c("FT-SEM","lm_parent","lm")))) +
-    geom_point(size = 2) +  # 添加点
+  RMSE <- ggplot(data, aes(x = Sample size, y = Offspring effect RMSE, group = Method,
+                           shape = factor(Method, levels = c("FT-SEM","lm_parent","lm")),
+                           linetype = factor(Method, levels = c("FT-SEM","lm_parent","lm")))) +
+    geom_point(size = 2) +  
     geom_line(linewidth = 0.8) +
     labs(x = "Sample size", y = "RMSE",shape = "Method",linetype = "Method") +
     # theme_minimal() +
     theme(
-      panel.grid = element_blank(), # 去除背景的方框线
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  # 在面板周围添加黑色实线边框
-      panel.background = element_rect(fill = "white", color = NA),  # 设置面板背景为白色
+      panel.grid = element_blank(), 
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  
+      panel.background = element_rect(fill = "white", color = NA),  
       axis.ticks = element_line(color = "black"),
-      axis.title = element_text(family = "sans",size = 9), # 加粗并增大轴标题字体
-      axis.text = element_text(family = "sans",size = 9),  # 增大坐标轴刻度字体
-      strip.text = element_text(family = "sans",size = 9), # 增大 facet 标题的字体大小并加粗
-      aspect.ratio = 1,  # 设置图形为正方形
-      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  # 设置标题居中，字体样式和大小
+      axis.title = element_text(family = "sans",size = 9), 
+      axis.text = element_text(family = "sans",size = 9),  
+      strip.text = element_text(family = "sans",size = 9), 
+      aspect.ratio = 1,  
+      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9), 
     ) +
-    scale_x_continuous(breaks = c(1000, 2000, 3000)) + # 设置横坐标刻度为1000、2000和3000
+    scale_x_continuous(breaks = c(1000, 2000, 3000)) + 
     scale_y_continuous(limits = c(-0.005,0.15)) +
     ggtitle(title)
   RMSE
@@ -68,25 +68,25 @@ RMSE_null_population <- RMSE_plot(population_null, "Residual population stratifi
 RMSE_null_none       <- RMSE_plot(none_null, "No bias")
 
 Coverage_plot <- function(data,title){
-  Coverage <- ggplot(data, aes(x = 样本量, y = 直接效应Coverage, group = 方法,
-                               shape = factor(方法, levels = c("FT-SEM","lm_parent","lm")),
-                               linetype = factor(方法, levels = c("FT-SEM","lm_parent","lm")))) +
-    geom_point(size = 2) +  # 添加点
+  Coverage <- ggplot(data, aes(x = Sample size, y = Offspring effect Coverage, group = Method,
+                               shape = factor(Method, levels = c("FT-SEM","lm_parent","lm")),
+                               linetype = factor(Method, levels = c("FT-SEM","lm_parent","lm")))) +
+    geom_point(size = 2) +  
     geom_line(linewidth = 0.8) +
     labs(x = "Sample size", y = "CP of CI",shape = "Method",linetype = "Method") +
     # theme_minimal() +
     theme(
-      panel.grid = element_blank(), # 去除背景的方框线
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  # 在面板周围添加黑色实线边框
-      panel.background = element_rect(fill = "white", color = NA),  # 设置面板背景为白色
+      panel.grid = element_blank(), 
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  
+      panel.background = element_rect(fill = "white", color = NA), 
       axis.ticks = element_line(color = "black"),
-      axis.title = element_text(family = "sans",size = 9), # 加粗并增大轴标题字体
-      axis.text = element_text(family = "sans",size = 9),  # 增大坐标轴刻度字体
-      strip.text = element_text(family = "sans",size = 9), # 增大 facet 标题的字体大小并加粗
-      aspect.ratio = 1,  # 设置图形为正方形
-      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  # 设置标题居中，字体样式和大小
+      axis.title = element_text(family = "sans",size = 9),
+      axis.text = element_text(family = "sans",size = 9),  
+      strip.text = element_text(family = "sans",size = 9), 
+      aspect.ratio = 1,  
+      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9), 
     ) +
-    scale_x_continuous(breaks = c(1000, 2000, 3000)) + # 设置横坐标刻度为1000、2000和3000
+    scale_x_continuous(breaks = c(1000, 2000, 3000)) + 
     scale_y_continuous(limits = c(-0.005,1)) +
     ggtitle(title)
   Coverage
@@ -96,25 +96,25 @@ Coverage_null_population <- Coverage_plot(population_null, "Residual population 
 Coverage_null_none       <- Coverage_plot(none_null, "No bias")
 
 CI_plot <- function(data,title){
-  CI <- ggplot(data, aes(x = 样本量, y = 直接效应CILength, group = 方法,
-                         shape = factor(方法, levels = c("FT-SEM","lm_parent","lm")),
-                         linetype = factor(方法, levels = c("FT-SEM","lm_parent","lm")))) +
-    geom_point(size = 2) +  # 添加点
+  CI <- ggplot(data, aes(x = Sample size, y = Offspring effect CILength, group = Method,
+                         shape = factor(Method, levels = c("FT-SEM","lm_parent","lm")),
+                         linetype = factor(Method, levels = c("FT-SEM","lm_parent","lm")))) +
+    geom_point(size = 2) +  
     geom_line(linewidth = 0.8) +
     labs(x = "Sample size", y = "Width of CI",shape = "Method",linetype = "Method") +
     # theme_minimal() +
     theme(
-      panel.grid = element_blank(), # 去除背景的方框线
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  # 在面板周围添加黑色实线边框
-      panel.background = element_rect(fill = "white", color = NA),  # 设置面板背景为白色
+      panel.grid = element_blank(), 
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  
+      panel.background = element_rect(fill = "white", color = NA),  
       axis.ticks = element_line(color = "black"),
-      axis.title = element_text(family = "sans",size = 9), # 加粗并增大轴标题字体
-      axis.text = element_text(family = "sans",size = 9),  # 增大坐标轴刻度字体
-      strip.text = element_text(family = "sans",size = 9), # 增大 facet 标题的字体大小并加粗
-      aspect.ratio = 1,  # 设置图形为正方形
-      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  # 设置标题居中，字体样式和大小
+      axis.title = element_text(family = "sans",size = 9), 
+      axis.text = element_text(family = "sans",size = 9),  
+      strip.text = element_text(family = "sans",size = 9), 
+      aspect.ratio = 1,  
+      plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  
     ) +
-    scale_x_continuous(breaks = c(1000, 2000, 3000)) + # 设置横坐标刻度为1000、2000和3000
+    scale_x_continuous(breaks = c(1000, 2000, 3000)) + 
     scale_y_continuous(limits = c(-0.005,0.5)) +
     ggtitle(title)
   CI
@@ -124,22 +124,22 @@ CI_null_population <- CI_plot(population_null, "Residual population stratificati
 CI_null_none       <- CI_plot(none_null, "No bias")
 
 Type_one_error_rate_plot <- function(data,title){
-  Type_one_error_rate <- ggplot(data, aes(x = 样本量, y = 直接效应第一类错误率, group = 方法,
-                                          shape = factor(方法, levels = c("FT-SEM","lm_parent","lm")),
-                                          linetype = factor(方法, levels = c("FT-SEM","lm_parent","lm")))) +
-    geom_point(size = 2) +  # 添加点
+  Type_one_error_rate <- ggplot(data, aes(x = Sample size, y = Offspring effect Type I error rates, group = Method,
+                                          shape = factor(Method, levels = c("FT-SEM","lm_parent","lm")),
+                                          linetype = factor(Method, levels = c("FT-SEM","lm_parent","lm")))) +
+    geom_point(size = 2) +  
     geom_line(linewidth = 0.8) +
     labs(x = "Sample size", y = "Type I error rate",shape = "Method",linetype = "Method") +
     # theme_minimal() +
     theme(
-      panel.grid = element_blank(), # 去除背景的方框线
-      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),  # 在面板周围添加黑色实线边框
-      panel.background = element_rect(fill = "white", color = NA),  # 设置面板背景为白色
+      panel.grid = element_blank(), 
+      panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8), 
+      panel.background = element_rect(fill = "white", color = NA),  
       axis.ticks = element_line(color = "black"),
-      axis.title = element_text(family = "sans",size = 9), # 加粗并增大轴标题字体
-      axis.text = element_text(family = "sans",size = 9),  # 增大坐标轴刻度字体
-      strip.text = element_text(family = "sans",size = 9), # 增大 facet 标题的字体大小并加粗
-      aspect.ratio = 1,  # 设置图形为正方形
+      axis.title = element_text(family = "sans",size = 9), 
+      axis.text = element_text(family = "sans",size = 9),  
+      strip.text = element_text(family = "sans",size = 9), 
+      aspect.ratio = 1,  
       plot.title = element_text(hjust = 0.5, family = "sans", face = "bold", size = 9),  # 设置标题居中，字体样式和大小
     ) +
     scale_x_continuous(breaks = c(1000, 2000, 3000)) + # 设置横坐标刻度为1000、2000和3000
